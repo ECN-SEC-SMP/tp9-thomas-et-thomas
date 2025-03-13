192f765ec29246eb712ecd5f0b4990e53d068730
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-03-13 14:15:05 +0100

    Add CSV file reader and writter + Comments

--- include/CSV.h
+++ include/CSV.h
@@ -1,3 +1,61 @@
 #pragma once
 
+#include <iostream>
+#include <fstream> 
 #include <string>
+#include <map>
+
+/**
+ * @brief Lit un fichier CSV et stocke son contenu dans une map.
+ * 
+ * @details
+ * Cette fonction lit un fichier CSV où chaque ligne contient une paire clé-valeur séparée par un point-virgule.
+ * Elle stocke les paires clé-valeur dans la map fournie.
+ * 
+ * @param filename Le chemin vers le fichier CSV à lire.
+ * @param data Une référence à une map où les paires clé-valeur seront stockées.
+ */
+void readCSV(std::string filename, std::map<int, std::string> &data)
+{
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Erreur : fichier non trouvé" << std::endl;
+        return;
+    }
+    data.clear();
+    for (std::string line; std::getline(file, line);)
+    {
+        size_t pos = line.find(";"); // Séparateur CSV
+        int key = std::stoi(line.substr(0, pos));
+        std::string value = line.substr(pos + 1, line.npos);
+        data[key] = value;
+    }
+    file.close();
+}
+
+/**
+ * @brief Écrit le contenu d'une map dans un fichier CSV.
+ * 
+ * @details
+ * Cette fonction écrit les paires clé-valeur de la map fournie dans un fichier CSV.
+ * Chaque paire clé-valeur est écrite sur une nouvelle ligne, séparée par un point-virgule.
+ * 
+ * @param filename Le chemin vers le fichier CSV à écrire.
+ * @param data Une référence à une map contenant les paires clé-valeur à écrire.
+ */
+void writeCSV(std::string filename, std::map<int, std::string> &data)
+{
+    std::ofstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Erreur : fichier non trouvé" << std::endl;
+        return;
+    }
+    file.clear();
+    for (auto &&pair : data)
+    {
+        file << pair.first << ";" << pair.second << std::endl;
+    }
+    file.close();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,43 +5,50 @@
 #include <string>
 
 #include "fonction.h"
+#include "CSV.h"
 
 int main(int argc, char const *argv[])
 {
+    // Créer un vecteur d'entiers
     std::vector<int> entiers;
     for (int i = 0; i < 3; i++)
     {
         entiers.push_back(i);
     }
-    for (int i = 0; i < entiers.size(); i++)
+    // Afficher le vecteur avec une boucle for et un index
+    for (size_t i = 0; i < entiers.size(); i++)
     {
         std::cout << entiers[i] << std::endl;
     }
     std::cout << "----------------" << std::endl;
 
+    // Afficher le vecteur avec une boucle basée sur la plage
     for (auto &&e : entiers)
     {
         std::cout << e << std::endl;
     }
     std::cout << "----------------" << std::endl;
 
+    // Afficher le vecteur avec un itérateur
     for (std::vector<int>::iterator it = entiers.begin(); it != entiers.end(); it++)
     {
         std::cout << *it << std::endl;
     }
     std::cout << "----------------" << std::endl;
-    
-    std::cout << "Size: " << entiers.size() << std::endl;
-    entiers.push_back(3);
-    std::cout << "Size: " << entiers.size() << std::endl;
 
-    
+    // Modifier la taille du vecteur en ajoutant un élément
+    std::cout << "Taille: " << entiers.size() << std::endl;
+    entiers.push_back(3);
+    std::cout << "Taille: " << entiers.size() << std::endl;
+
+    // Copier un vecteur
     std::vector<int> entiers_copie;
     std::copy(entiers.begin(), entiers.end(), std::back_inserter(entiers_copie));
 
     // ----------------------------------------------------------------------------------
     std::cout << "----------------------------------------------------------------------------------" << std::endl;
 
+    // Créer un vecteur de chaînes de caractères
     std::vector<std::string> string;
     string.push_back("Bonjour");
     string.push_back("comment");
@@ -57,13 +64,15 @@ int main(int argc, char const *argv[])
 
     std::cout << "----------------" << std::endl;
 
-    for (int i = 0; i < string.size(); i++)
+    // Afficher le vecteur avec une boucle for et un index
+    for (size_t i = 0; i < string.size(); i++)
     {
         std::cout << string[i] << " ";
     }
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Afficher le vecteur avec une boucle basée sur la plage
     for (auto &&s : string)
     {
         std::cout << s << " ";
@@ -71,6 +80,7 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Afficher le vecteur avec un itérateur
     for (std::vector<std::string>::iterator it = string.begin(); it != string.end(); it++)
     {
         std::cout << *it << " ";
@@ -78,8 +88,10 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Échanger les 2ème et 4ème éléments
     string[1].swap(string[3]);
 
+    // Afficher le vecteur avec un itérateur
     for (std::vector<std::string>::iterator it = string.begin(); it != string.end(); it++)
     {
         std::cout << *it << " ";
@@ -87,8 +99,10 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Insérer un nouvel élément à la 2ème position
     string.insert(string.begin()+1, "a tous");
 
+    // Afficher le vecteur avec un itérateur
     for (std::vector<std::string>::iterator it = string.begin(); it != string.end(); it++)
     {
         std::cout << *it << " ";
@@ -96,9 +110,12 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Supprimer le dernier élément
    string.pop_back();
+   // Ajouter la ponctuation correcte à la fin
    string.push_back("???");
 
+   // Afficher le vecteur avec un itérateur
     for (std::vector<std::string>::iterator it = string.begin(); it != string.end(); it++)
     {
         std::cout << *it << " ";
@@ -106,6 +123,7 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Afficher le vecteur avec un itérateur avec des virgules
     for (std::vector<std::string>::iterator it = string.begin(); it != string.end(); it++)
     {
         std::cout << *it << ",";
@@ -113,9 +131,10 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
-
+    // Trier le vecteur
     std::sort(string.begin(),string.end());
 
+    // Afficher le vecteur avec un itérateur
     for (std::vector<std::string>::iterator it = string.begin(); it != string.end(); it++)
     {
         std::cout << *it << " ";
@@ -123,32 +142,59 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
 
+    // Afficher le vecteur avec la fonction afficheVector()
     afficheVector(string);
 
+    // Concaténer tous les éléments du vecteur et afficher le résultat
     std::cout << concatener(string) << std::endl;
 
+    // Ajouter une virgule après chaque élément du vecteur et afficher le résultat
     ajoute_virgule(string);
+    // Afficher le vecteur avec la fonction afficheVector()
     afficheVector(string);
 
-    // ----------------------------------------------------------------------------------
     std::cout << "----------------------------------------------------------------------------------" << std::endl;
 
+    // Créer une liste
     std::list<int> l = {7, 5, 16, 8, 22, 1, 3, 30};
+    // Afficher la liste
     for (auto &&e : l)
     {
         std::cout << e << " ";
     }
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
-    auto it = l.begin();
-    std::advance(it, 2);
-    l.erase(it);
+    
+    // Supprimer le 3ème élément
+    auto it = l.begin(); // Obtenir un itérateur vers le premier élément
+    std::advance(it, 2); // Ajouter 2 à l'itérateur
+    l.erase(it); // Supprimer le 3ème élément
+
+    // Afficher la liste à nouveau
     for (auto &&e : l)
     {
         std::cout << e << " ";
     }
     std::cout << std::endl;
     std::cout << "----------------" << std::endl;
+
+    std::cout << "----------------------------------------------------------------------------------" << std::endl;
+
+    // Lire les dates à partir d'un fichier CSV
+    std::map<int, std::string> dates;
+    readCSV("dates.csv", dates);
+    for (auto &&pair : dates)
+    {
+        std::cout << pair.first << " : " << pair.second << std::endl;
+    }
+    std::cout << "----------------" << std::endl;
+
+    // Écrire de nouvelles données dans un fichier CSV
+    std::map<int, std::string> new_data;
+    new_data[2020] = "Covid-19";
+    new_data[2021] = "Vaccin";
+    new_data[2022] = "Confinement";
+    writeCSV("new_data.csv", new_data);
     
     return 0;
 }
